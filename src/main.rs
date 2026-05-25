d9507b816b074cdff8d6c59da2d839462482e2f8
Jorgelzn <jorgelizgo@gmail.com>
2026-05-25 11:43:56 +0200

    add canvas

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use ratatui::style::Stylize;
+use ratatui::style::{Stylize,Color};
 use ratatui::widgets::{Block, Paragraph};
-
+use ratatui::widgets::canvas::{Canvas, Line, Map, MapResolution, Rectangle};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     ratatui::run(|terminal| {
         terminal.draw(|frame| {
@@ -9,7 +9,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .centered()
                 .yellow()
                 .block(block);
-            frame.render_widget(greeting, frame.area());
+            let canvas = Canvas::default()
+                .block(Block::bordered().title("Canvas"))
+                .x_bounds([-180.0, 180.0])
+                .y_bounds([-90.0, 90.0])
+                .paint(|ctx| {
+                    ctx.draw(&Map {
+                        resolution: MapResolution::High,
+                        color: Color::White,
+                    });
+                    ctx.layer();
+                    ctx.draw(&Line {
+                        x1: 0.0,
+                        y1: 10.0,
+                        x2: 10.0,
+                        y2: 10.0,
+                        color: Color::White,
+                    });
+                    ctx.draw(&Rectangle {
+                        x: 10.0,
+                        y: 20.0,
+                        width: 10.0,
+                        height: 10.0,
+                        color: Color::Red,
+                    });
+                });
+            frame.render_widget(canvas, frame.area());
         })?;
         std::thread::sleep(std::time::Duration::from_secs(5));
         Ok(())
