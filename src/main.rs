1129f0108c126f8c188963eb000409ee0106a283
Jorgelzn <jorgelizgo@gmail.com>
2026-05-25 16:25:19 +0200

    refactor in files

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,7 @@
-use ratatui::style::{Color};
-use ratatui::widgets::{Block};
-use ratatui::widgets::canvas::{Canvas, Line, Map, MapResolution, Rectangle};
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ratatui::run(|myterminal| {
-        myterminal.draw(|myframe| {
-            let canvas = Canvas::default()
-                .block(Block::bordered().title("Canvas"))
-                .x_bounds([-180.0, 180.0])
-                .y_bounds([-90.0, 90.0])
-                .paint(|ctx| {
-                    ctx.draw(&Map {
-                        resolution: MapResolution::High,
-                        color: Color::White,
-                    });
-                    ctx.layer();
-                    ctx.draw(&Rectangle {
-                        x: 10.0,
-                        y: 20.0,
-                        width: 10.0,
-                        height: 10.0,
-                        color: Color::Red,
-                    });
-                    ctx.layer();
-                    ctx.draw(&Line {
-                        x1: 0.0,
-                        y1: 10.0,
-                        x2: 100.0,
-                        y2: 100.0,
-                        color: Color::Blue,
-                    });
-                });
-            myframe.render_widget(canvas, myframe.area());
-        })?;
-        std::thread::sleep(std::time::Duration::from_secs(5));
-        Ok(())
-    })
+use std::io;
+mod app;
+use app::App;
+
+fn main() -> io::Result<()> {
+    ratatui::run(|terminal| App::default().run(terminal))
 }
