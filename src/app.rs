85d99638ee70963235217b2a83aba2d52e34d690
Jorgelzn <jorgelizgo@gmail.com>
2026-05-26 17:01:00 +0200

    stablish canvas

--- src/app.rs
+++ src/app.rs
@@ -84,6 +84,7 @@ impl App {
 
     fn zoom_up(&mut self) {
         self.zoom += 0.1;
+        
     }
 
     fn zoom_down(&mut self) {
@@ -115,17 +116,16 @@ impl Widget for &App {
             .border_set(border::THICK);
 
         canvas::Canvas::default()
-        .x_bounds([-x_bound, x_bound])
-        .y_bounds([-y_bound, y_bound])
+        .x_bounds([-x_bound+self.posx, x_bound+self.posx])
+        .y_bounds([-y_bound+self.posy, y_bound+self.posy])
         .marker(Marker::Braille)
         .paint(|ctx| {
-            ctx.draw(&canvas::Circle {
-                x: self.posx,
-                y: self.posy,
-                radius: 1.0,
-                color: Color::Red,
+                        ctx.draw(&canvas::Map {
+                resolution: canvas::MapResolution::High,
+                color: Color::White,
             });
-        }).block(block)
+        })
+        .block(block)
         .render(area, buf);
     }
 
@@ -139,50 +139,5 @@ impl Default for App {
             posy: 0.0,
             zoom: 1.0
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ratatui::style::Style;
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        assert_eq!(buf, expected);
-    }
-
-    #[test]
-    fn handle_key_event() {
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
-
-        app.handle_key_event(KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
-        assert!(app.exit);
     }
 }
